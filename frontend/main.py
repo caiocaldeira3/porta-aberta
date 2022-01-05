534427f4d3371014e4f9e89fbc3f29facd54cc31
Luiz Romanhol <luizromanhol@ufmg.br>
2022-01-05 02:17:57 -0300

    Foram feitos o HTML e CSS da pagina Home e a pagina Index foi ajustada. Apesar disso ainda falta muito da programação Flask no frontend.

--- frontend/main.py
+++ frontend/main.py
@@ -6,14 +6,21 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'password'
-@app.route('/')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+    
+@app.route("/",methods=['GET', 'POST'])
 def index():
+    print("TAMO RODANDOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
     if request.method == 'POST':
         email = request.form["user"]
         passwd = request.form["passwd"]
+        print("RODEIaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+        return redirect(url_for('home'))
 
     return render_template('index.html')
-    
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8085, debug=True)
+    app.run(host='127.0.0.1', port=8080, debug=True)
